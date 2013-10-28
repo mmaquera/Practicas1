12e90f9c59749c33f0d596ece4fbd4362fb2a8f2
mmaquera <marco.arka@gmail.com>
2013-10-28 17:59:25 -0500

    Optimizacion
    Ajustando pixeles

--- djangomaquetacion/app/admin.py
+++ djangomaquetacion/app/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from models import Menu, Articulo, Comentario,Exposicion,BannerInicio,Nosotro,BannerNosotro,Galeria,Taller,DatosEmpresa,Artista,BannerArtista
 
+class ArticuloAdmin(admin.ModelAdmin):
+	list_display = ('Titulo','Descripcion',)
+
+class ComentarioAdmin(admin.ModelAdmin):
+	list_display = ('Articulo','Descripcion','Autor',)
+
+
 admin.site.register(Menu)
-admin.site.register(Articulo)
-admin.site.register(Comentario)
+admin.site.register(Articulo,ArticuloAdmin)
+admin.site.register(Comentario, ComentarioAdmin)
 admin.site.register(Exposicion)
 admin.site.register(BannerInicio)
 admin.site.register(Nosotro)
@@ -13,3 +20,5 @@ admin.site.register(Taller)
 admin.site.register(DatosEmpresa)
 admin.site.register(Artista)
 admin.site.register(BannerArtista)
+
+
